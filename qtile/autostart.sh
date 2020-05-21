e66a253b2b8af4febc24b2f0b10d7690964550e1
angelip2303 <65736636+angelip2303@users.noreply.github.com>
2020-05-21 23:06:50 +0200

    Add files via upload

---
+++ qtile/autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# -*- WALLPAPERS -*-
+
+# feh --bg-scale ~/.config/qtile/wallpapers/wallpaper1.jpg &
+feh --bg-scale ~/.config/qtile/wallpapers/wallpaper2.png &
+
+# -*- STARTUP -*-
+
+exec picom --config ~/.config/picom/picom.conf &
